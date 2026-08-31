28974d6592b1a888517a9d10a0d5b872c67fd0b0
Oleg Bragin <oleg.bragin@icloud.com>
2026-08-31 19:09:16 +0300

    New architecture

--- Packages/DSKit/Sources/DSKit/Colors.swift
+++ Packages/DSKit/Sources/DSKit/Colors.swift
@@ -8,6 +8,7 @@ public extension Color {
         public static let colorForeground = Color("colorForeground", bundle: .module)
         public static let colorForegroundDisabled = Color("colorForegroundDisabled", bundle: .module)
         public static let colorForegroundEvent = Color("colorForegroundEvent", bundle: .module)
+        public static let colorForegroundOnEventCard = Color("colorForegroundOnEventCard", bundle: .module)
         public static let colorEventOption1 = Color("eventColorOption1", bundle: .module)
         public static let colorEventOption2 = Color("eventColorOption2", bundle: .module)
         public static let colorEventOption3 = Color("eventColorOption3", bundle: .module)
